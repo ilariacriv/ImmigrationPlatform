0f273f69375cb3ed6b3413ce71f80968714056a9
ilariacriv <58052642+ilariacriv@users.noreply.github.com>
2024-10-17 10:38:45 +0200

    feat-#9: create list component

--- src/components/TopicInfo.js
+++ src/components/TopicInfo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './TopicInfo.css';
 import { useTranslation } from 'react-i18next';
 import TopicInfoToggle from './TopicInfoToggle';
+import TextList from './TextList';
 
 const TopicInfo = (props) => {
     const { t } = useTranslation(`${props.ns}`);
@@ -10,14 +11,20 @@ const TopicInfo = (props) => {
         <TopicInfoToggle key={index} topicid={props.topicid} num={index+1} ns={props.ns}/>
       ));
 
+    const listKey = `${props.topicid}.list`;
+    const hasList = t(listKey, { returnObjects: true, defaultValue: false });
+    
     return (
         <div className="topic-info-container">
-        <div className="topic-info">
-            <div className="title">{t(`${props.topicid}.titletext`)}</div>
-            <div className="separator"></div>
-            <div className="content">{t(`${props.topicid}.details`)} </div>
-        </div>
-        {toggles}
+            <div className="topic-info">
+                <div className="title">{t(`${props.topicid}.titletext`)}</div>
+                <div className="separator"></div>
+                <div className="content">{t(`${props.topicid}.details`)}
+                {hasList && <TextList ns={props.ns} path={props.topicid}/>}
+                </div>
+                
+            </div>
+            {toggles}
         </div>
     );
 };
